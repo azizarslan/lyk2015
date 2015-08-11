dcf93e7cdd5a5dc52f59762f8eb2b134b0da2b4a
Aziz Arslan <arslanbilisimnet@gmail.com>
2015-08-11 11:34:22 +0300

    Added jquery-basic

---
+++ jquery-basic/js/script.js
@@ -0,0 +1,22 @@
+/* document.getElementsByClassName('deneme')[0].setAttribute("style", "display: none");
+
+$(".deneme").hide(); */
+
+
+/* document.getElementsByClassName('deneme')[0].innerHTML = "selam";
+
+$(".deneme").html("selam"); */
+
+/* document.getElementsByClassName('deneme')[0].className = document.getElementsByClassName('deneme')[0].className + " yenibirdeneme";
+
+$(".deneme").addClass("yenibirdeneme"); */
+
+$(".menulink").click(function() {
+  $(".menu").toggle();
+
+  /* if ($(".menu").is(":visible")) {
+    $(".menu").hide();
+  } else {
+    $(".menu").show();
+  } */
+});

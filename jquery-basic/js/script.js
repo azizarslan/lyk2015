84d48f3c9cf264e16edeb113d189b0896e41d01d
Aziz Arslan <arslanbilisimnet@gmail.com>
2015-08-11 20:58:20 +0300

    Updated jquery-basic folder

--- jquery-basic/js/script.js
+++ jquery-basic/js/script.js
@@ -7,9 +7,11 @@ $(".deneme").hide(); */
 
 $(".deneme").html("selam"); */
 
+
 /* document.getElementsByClassName('deneme')[0].className = document.getElementsByClassName('deneme')[0].className + " yenibirdeneme";
 
 $(".deneme").addClass("yenibirdeneme"); */
+
 
 $(".menulink").click(function() {
   $(".menu").toggle();
@@ -20,3 +22,63 @@ $(".menulink").click(function() {
     $(".menu").show();
   } */
 });
+
+$(".forminput").focus(function() {
+  console.log("Focus oldu.");
+});
+
+$(".forminput").focusout(function() {
+  if ($(".forminput").val() == "") {
+    console.log("Lütfen bir değer girin.");
+  }
+
+  // console.log("FocusOut oldu.");
+});
+
+$(".forminput").keyup(function() {
+  var value = $(this).val();
+
+  $(".formverisi").html(value);
+});
+
+
+$(".content a").mouseover(function() {
+  var content = $(this).attr("data-tooltip");
+  $(this).append('<div class="tooltip">' + content + '</div>');
+});
+
+$(".content a").mousemove(function(event) {
+  $(".tooltip").css({
+    "top": event.pageY + 20,
+    "left": event.pageX + 20
+  });
+});
+
+$(".content a").mouseout(function() {
+  $(this).children(".tooltip").remove();
+});
+
+
+
+
+
+
+
+
+
+
+/* localStorage.setItem("isim", "Aziz");
+console.log(localStorage.getItem("isim"));
+localStorage.removeItem("isim"); */
+
+
+$(".person").submit(function(event) {
+  event.preventDefault();
+
+  localStorage.setItem("firstname", $("input.firstname").val());
+  localStorage.setItem("lastname", $("input.lastname").val());
+});
+
+if (localStorage.getItem("firstname") && localStorage.getItem("lastname")) {
+  console.log("Hoşgeldin " + localStorage.getItem("firstname") + " " + localStorage.getItem("lastname"));
+}
